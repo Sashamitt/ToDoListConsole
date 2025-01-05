ce3a978d548a5be22447d1da07fbb4e98627bcfd
SasiKudasi <sasha.mittal@yandex.ru>
2025-01-05 18:30:48 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Выберите действие");
+        System.out.println("1. Добавить задачу");
+        System.out.println("2. Просмотр задач");
+        System.out.println("3. Удалить задачу");
+        System.out.println("4. Удалить все задачи");
+        System.out.println("5. Изменить задачу");
+       // var scan = new Scanner(System.in);
+        while (true){
+
+
+        }
+    }
+
+
+    }
